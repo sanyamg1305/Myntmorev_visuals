2d2a1cbbb4af379073350a14491eb69c2691fd89
sanyamg1305 <sanyam.raj.golechha@gmail.com>
2025-06-16 10:13:26 +0530

    Add files via upload

--- visuals.py
+++ visuals.py
@@ -3,12 +3,27 @@ import pandas as pd
 from io import BytesIO
 import os
 import json
-from google.oauth2 import service_account
+from oauth2client.service_account import ServiceAccountCredentials
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
 # Constants
 DRIVE_FOLDER_ID = "12qxuWIpFEAwgiVjY2AmjqT9nQ4vn8Og4"
+
+# Secret JSON credentials
+SERVICE_ACCOUNT_CREDS = {
+  "type": "service_account",
+  "project_id": "myntmoremetrics",
+  "private_key_id": "3099e8c12eed41bc49374f1c0751499069905787",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChcqnDA9xFqgDB\n91BlD1wqOz/PmljnDDDtQbnkSELob2RpVAvyGHtX4lGJoZIQ2ErUJVxOUbR5gzdO\neMQt/UHS9IimvJAQ+xne5FCF0Rn3tQkVIi+t89EPOE5TNv+P0RQl1uLPYq7hJ+a7\nrJSk/d2At17SRU6LQ8ulYiMTytv472JmvFa3Sr0kSEUUn6VzwX1dxs/tHFzSqz66\ny/nJtGsQbBpFfUKFyTZTM/UX2gHUhUsNFewt/QYvcUPlCccH3BMj8gJXNQKiHlm2\n0XQEakJxqIlym+FtuK9L3rG+RTW/ClsUaZ0/Z4y8Njl/m3FscL9qvNg2pU7dYeB7\nF1/NiRYHAgMBAAECggEAGIzPbxMwNKZe9R/UFCSwzCSJqy+k+IVXD0z2T9YD2UUk\nYgvgwSX4jkBLoSp2LqTlRBCQ/RaEOLy7n+18pM2lFnIO3DlCOFgmnUk0oWn5kTxF\n2aEthOiAJYNXYw9dmeieMAmBddhjZWOO54JHxuBqWqoHpjaEdL2Qwwutmn3iKJ6S\nwUjRd2t5Fyf9AO4SjLYa66HG4OzqIngFT0yRZg+7ur4al+2B2XqEfcwNJhgHXM7l\n+3Lu40ZqsxPxb5DgyatDWdvgr8qtPO/rTBjwTqo1thIJL6PVt2dp9BxIyaGBqJrT\nYVN38BkiihrbMCb5pQNcuLyczCMS8rNNMynBX2GLCQKBgQDguSkR+bTVzz8hwzfk\nLPSMIAhLAvRE//ZFx/+KdEpho+hTzQDxmyGcwtid0MSlHwfOlfBGuB7o/1WXNHZm\nIvQrhNZg2hXK4+8wmilhJ4pE+Konux9ppDFH6Dtalt7A2cPKTt4v6nP7YviRqmOs\nGxZzy4geXqq6filTkedRwKDigwKBgQC36wPouleC0uOCW5wwfH9swbzp1YjIKaYf\nLm69nXdZQlbJX2K6UM6ljVzvW0uljmlnRbVAd1mHgdk7UI3Ak8OdiszBCKagxlT3\ntF1uNi32FZhtXtGOITvPutFTLtedP7hLzbNDP6MNmtRrsCMUckAkn+qpZq4YVogp\nJKPRXOKXLQKBgQCYFLb2s0bua5MOBk+M29+j9QmnuhgVmiPQlckaqhise4B/Nf2t\nhI76x+JQ6zgphxaBeHdjZLeGd0Y1TgvSk1UrHnr2kKCu7hKxkaLvXRL2GlMNEFXx\n6GxxfXitleyqFrWp9DXpt4FGX3RZIDbUAoDvXI6B/w8LptXQ+KzTZsl6gQKBgB2P\nGwi7cjcnd8NKv/aW+8/Z72fRlvM8pmNajhfRiA4DeHlS6EYzmJLI/ofFr4nDMrOT\nY4ch53S87NH4p1+tIAJg/XmEz2sFlUBeb6m38XkcjWv+kkV6l+dSCjSJUcQHKrKc\nM9R6FNWp7bwFCg5OMnrwbMGfLBnjRmz+Y4nRmOVdAoGAAQRja94E8QjCe8KXtD9l\nWBNVQI4amowXyGY9sLQCPT0qDBcsVELqg0XKGixv3Q3gecZYPdaBjVOTGcN2HYtv\nY5OyIss03Xcj6TUHJphZjZD4shJk38D6qDOCyf2Pbh1/ium47zBedmX6+z7+Llsd\naWjQvXHGDQEA+qFwjD3W5eU=\n-----END PRIVATE KEY-----\n",
+  "client_email": "metrics@myntmoremetrics.iam.gserviceaccount.com",
+  "client_id": "110536428410191620844",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/metrics%40myntmoremetrics.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 
 st.set_page_config(page_title="MyntMetrics Dashboard", layout="wide")
 st.title("📊 MyntMetrics: Multi-Month Metrics Dashboard")
@@ -16,14 +31,8 @@ st.title("📊 MyntMetrics: Multi-Month Metrics Dashboard")
 # Authenticate Google Drive
 @st.cache_resource(show_spinner=False)
 def authenticate_drive():
-    with open("myntmoremetrics-3099e8c12eed.json") as f:
-        service_account_info = json.load(f)
-
-    credentials = service_account.Credentials.from_service_account_info(
-        service_account_info,
-        scopes=['https://www.googleapis.com/auth/drive']
-    )
-
+    scope = ['https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(SERVICE_ACCOUNT_CREDS, scopes=scope)
     gauth = GoogleAuth()
     gauth.credentials = credentials
     return GoogleDrive(gauth)
